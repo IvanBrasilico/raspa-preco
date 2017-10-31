e3b68a4015ca89737609b7ffd244de462430bf80
IvanBrasilico <brasilico.ivan@gmail.com>
2017-10-31 12:21:17 -0200

    Test REST - manytomany e final index

--- raspapreco/tests/rest_test.py
+++ raspapreco/tests/rest_test.py
@@ -87,7 +87,7 @@ class FlaskTestCase(unittest.TestCase):
         produtoid = self._postproduto()
         descricao = self._getproduto(produtoid)
         self._queryproduto(descricao)
-        # self._put(siteid)
+        self._putproduto(produtoid)
         self._deleteproduto(produtoid)
 
     def _postproduto(self):
@@ -119,8 +119,94 @@ class FlaskTestCase(unittest.TestCase):
         fields = json.loads(rv.data.decode('utf-8'))
         assert fields['objects'][0]['descricao'].find(descricao) != -1
 
+    def _putproduto(self, id):
+        rv = self.app.put('/api/produtos/' + str(id), data=json.dumps({'descricao': 'test2'}),
+                          follow_redirects=True,
+                          headers={'Content-Type': 'application/json'})
+        fields = json.loads(rv.data.decode('utf-8'))
+        assert b'"descricao": "test2"' in rv.data
+        return fields['id']
+
     def _deleteproduto(self, produtoid):
         rv = self.app.delete('/api/produtos/' + str(produtoid), follow_redirects=True,
+                             headers={'Content-Type': 'application/json'})
+        print(rv.data)
+        # fields = json.loads(rv.data.decode("utf-8"))
+        assert rv.data == b''
+
+    def test_apiprocedimento(self):
+        id = self._postprocedimento()
+        campo = self._getprocedimento(id)
+        self._queryprocedimento(campo)
+        self._putprocedimento(id)
+        siteid = self._post()
+        produtoid = self._postproduto()
+        self._putprocedimentosite(id, siteid)
+        self._putprocedimentoproduto(id, produtoid)
+        self._delete(siteid)
+        self._deleteproduto(produtoid)
+        self._deleteprocedimento(id)
+
+    def _postprocedimento(self):
+        rv = self.app.post('/api/procedimentos', data=json.dumps({'nome': 'test'}),
+                           follow_redirects=True,
+                           headers={'Content-Type': 'application/json'})
+        fields = json.loads(rv.data.decode('utf-8'))
+        assert b'"nome": "test"' in rv.data
+        return fields['id']
+
+    def _getprocedimento(self, id):
+        rv = self.app.get('/api/procedimentos/' + str(id), follow_redirects=True,
+                          headers={'Content-Type': 'application/json'})
+        fields = json.loads(rv.data.decode('utf-8'))
+        assert id == fields['id']
+        assert fields['nome'] == 'test'
+        return fields['nome']
+
+    def _queryprocedimento(self, campo):
+        headers = {'Content-Type': 'application/json'}
+        filters = [dict(name='nome', op='like', val='%' + campo + '%')]
+        params = dict(q=json.dumps(dict(filters=filters)))
+        params = urlencode(params)
+        url = '/api/procedimentos?%s' % params
+        rv = self.app.get(url,
+                          headers=headers)
+        print(rv.data)
+        fields = json.loads(rv.data.decode('utf-8'))
+        assert fields['objects'][0]['nome'].lower().find(campo.lower()) != -1
+
+    def _putprocedimento(self, id):
+        rv = self.app.put('/api/procedimentos/' + str(id), data=json.dumps({'nome': 'test2'}),
+                          follow_redirects=True,
+                          headers={'Content-Type': 'application/json'})
+        fields = json.loads(rv.data.decode('utf-8'))
+        assert b'"nome": "test2"' in rv.data
+        return fields['id']
+
+    def _putprocedimentosite(self, id, siteid):
+        rv = self.app.put('/api/procedimentos/' + str(id),
+                          data=json.dumps({'sites':
+                                           {'add': [{'id': siteid}]}
+                                           }),
+                          follow_redirects=True,
+                          headers={'Content-Type': 'application/json'})
+        fields = json.loads(rv.data.decode('utf-8'))
+        # assert b'"nome": "test2"' in rv.data
+        return fields['id']
+
+    def _putprocedimentoproduto(self, id, produtoid):
+        rv = self.app.put('/api/procedimentos/' + str(id),
+                          data=json.dumps({'produtos':
+                                           {'add': [{'id': produtoid}]}
+                                           }),
+                          follow_redirects=True,
+                          headers={'Content-Type': 'application/json'})
+        fields = json.loads(rv.data.decode('utf-8'))
+        # assert b'"nome": "test2"' in rv.data
+        return fields['id']
+
+    def _deleteprocedimento(self, id):
+        rv = self.app.delete('/api/procedimentos/' + str(id), follow_redirects=True,
                              headers={'Content-Type': 'application/json'})
         print(rv.data)
         # fields = json.loads(rv.data.decode("utf-8"))
